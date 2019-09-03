1d3475a4b79a099cec26b0397fe3a5dce2126c3d
Kyle Clabough <kaclabough@gmail.com>
2019-09-03 12:23:04 -0400

    added background

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,53 +1,77 @@
 import React, { Component } from "react";
-
+import "./Projects.css";
 class Project extends Component {
   render() {
     return (
-      <div>
+      <div className="projects-page">
         <h2>These are the projects I have worked on</h2>
-        <ul>
-          <li>
-            <h3>Simon Says</h3>
-            Play the game{" "}
-            <a href="https://sirairdude.github.io/ga-project1-simon/">here</a>
-            <br />
-            Repo is hosted
-            <a href="https://github.com/SirAirdude/ga-project1-simon"> here</a>
-          </li>
+        <div>
+          <div className="project">
+            <h3 className="project-name">Simon Says</h3>
+            <p className="project-description">
+              A web page for playing a virtual version of the game{" "}
+              <span className="digimon">Simon</span>
+            </p>
+            <p className="project-hosting">
+              Play the game{" "}
+              <a href="https://sirairdude.github.io/ga-project1-simon/">here</a>
+              <br />
+              Repo is hosted
+              <a href="https://github.com/SirAirdude/ga-project1-simon">
+                {" "}
+                here
+              </a>
+            </p>
+            <div id="simon" className="project-image" />
+          </div>
           <br />
-          <li>
-            <h3>
-              Database API for Digimon Cyber Sleuth <br />
-            </h3>
-            API is hosted
-            <a href="https://digimon-cyber-sleuth-api.herokuapp.com/api/digimon">
-              here
-            </a>
-            <br />
-            Repo is hosted
-            <a href="https://github.com/SirAirdude/GA-Project-2-DSCS-API">
-              {" "}
-              here
-            </a>
-          </li>
+          <div className="project">
+            <h3 className="project-name digimon">Digimon: CyberSleuth API</h3>
+            <p className="project-description">
+              An API that contains information on all of the Digimon that appear
+              in the game <span className="digimon">Digimon: Cyber Sleuth</span>
+            </p>
+            <p className="project-hosting">
+              API is hosted
+              <a href="https://digimon-cyber-sleuth-api.herokuapp.com/api/digimon">
+                here
+              </a>
+              <br />
+              Repo is hosted
+              <a href="https://github.com/SirAirdude/GA-Project-2-DSCS-API">
+                {" "}
+                here
+              </a>
+            </p>
+            <div className="project-image" id="digimon">
+              placeholder
+            </div>
+          </div>
           <br />
-          <li>
-            <h3>
-              AnimeMe: a website for anime synopsis and tracking user favorites{" "}
-            </h3>
-            Website is hosted <a href="http://animeme.surge.sh/">here</a>
-            <br />
-            Repo hosted here
-            <a href="https://github.com/KeeneWong/Animeme">here</a>
-            <br />
-            API is hosted <a href="https://animeme-api.herokuapp.com/">here</a>
-            <br />
-            Repo is hosted{" "}
-            <a href="https://github.com/SirAirdude/ga-project-3-animeDB-api">
-              here
-            </a>
-          </li>
-        </ul>
+          <div className="project">
+            <h3 className="project-name">AnimeMe:</h3>
+            <p className="project-description">
+              a website for anime synopsis and tracking user favorites{" "}
+            </p>
+            <p className="project-hosting">
+              Website is hosted <a href="http://animeme.surge.sh/">here</a>
+              <br />
+              Repo hosted here
+              <a href="https://github.com/KeeneWong/Animeme">here</a>
+              <br />
+              API is hosted{" "}
+              <a href="https://animeme-api.herokuapp.com/">here</a>
+              <br />
+              Repo is hosted{" "}
+              <a href="https://github.com/SirAirdude/ga-project-3-animeDB-api">
+                here
+              </a>
+            </p>
+            <div className="project-image" id="animeme">
+              placeholder
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
